a71e0719ac1e1470e76003574b9df8a2d76f4ce3
whatever127 <mkuba50@outlook.com>
2019-08-23 16:54:22 +0200

    Remove information about virtual editions in unsupported builds

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 // Website information
-$websiteVersion = '3.29.0';
+$websiteVersion = '3.29.1';
 $requiredApi = '1.25.0';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
