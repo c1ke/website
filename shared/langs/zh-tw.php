aa15e6021a104cace0628b630d551808833b7f33
pan93412 <pan93412@gmail.com>
2019-08-23 23:22:27 +0800

    i18n: Update the translations of zh-tw.php.

--- shared/langs/zh-tw.php
+++ shared/langs/zh-tw.php
@@ -1,158 +1,161 @@
 <?php
 /*
-UUP dump translation file.
+UUP dump translation file for Chinese (Traditional).
+ - UUP dump 正體中文翻譯檔
 
 Translation information:
-English language name: English (United States)
-Localized language name: English (United States)
-Language code: en-US
-Authors: whatever127 and mspaintmsi
+English language name: Chinese (Traditional)
+Localized language name: 中文（正體）
+Language code: zh-TW
+Authors: pan93412
 */
 
 //Language information
-$s['code'] = 'en-US';
-$s['timeZone'] = 'UTC'; //Supported timezones: https://www.php.net/manual/en/timezones.php
+$s['code'] = 'zh-TW';
+$s['timeZone'] = 'Asia/Taipei'; //Supported timezones: https://www.php.net/manual/en/timezones.php
 
 //shared strings
 $s['uupdump'] = 'UUP dump';
 $s['uupdumpSub'] = '%s - UUP dump'; //Browse known builds - UUP dump
-$s['build'] = 'Build';
-$s['arch'] = 'Architecture';
-$s['ring'] = 'Ring';
-$s['updateid'] = 'Update ID';
-$s['update'] = 'Update';
-$s['lang'] = 'Language';
-$s['edition'] = 'Edition';
-$s['seachForBuilds'] = 'Search for builds...';
-$s['no'] = 'No';
-$s['yes'] = 'Yes';
-$s['yesRecommended'] = 'Yes (recommended)';
-$s['next'] = 'Next';
-$s['ok'] = 'OK';
-$s['cancel'] = 'Cancel';
-$s['information'] = 'Information';
-$s['totalDlSize'] = 'Total download size';
-$s['file'] = 'File';
-$s['expires'] = 'Expires';
+$s['build'] = '組建';
+$s['arch'] = '架構';
+$s['ring'] = '振鈴 (Ring)'; // TRANSLATOR: 我使用 Windows 中國的翻譯代替。若台灣區有對應翻譯，也請幫忙補上，謝謝！:)
+$s['updateid'] = '更新 ID';
+$s['update'] = '更新';
+$s['lang'] = '語言';
+$s['edition'] = '版本';
+$s['seachForBuilds'] = '搜尋組建...';
+$s['no'] = '否';
+$s['yes'] = '是';
+$s['yesRecommended'] = '確定 (建議)';
+$s['next'] = '下一步';
+$s['ok'] = '確定';
+$s['cancel'] = '取消';
+$s['information'] = '資訊';
+$s['totalDlSize'] = '總計下載大小';
+$s['file'] = '檔案';
+$s['expires'] = '過期時間';
 $s['sha1'] = 'SHA-1';
-$s['size'] = 'Size';
-$s['additionalEdition'] = 'Additional edition';
-$s['requiredEdition'] = 'Required edition';
-$s['unknown'] = 'Unknown';
+$s['size'] = '大小';
+$s['additionalEdition'] = '額外版本';
+$s['requiredEdition'] = '必須版本';
+$s['unknown'] = '未知';
 
 //global
-$s['home'] = 'Home';
-$s['downloads'] = 'Downloads';
-$s['lightMode'] = 'Light mode';
-$s['darkMode'] = 'Dark mode';
-$s['sourceCode'] = 'Source code';
-$s['menu'] = 'Menu';
-$s['websiteDesc'] = 'Download UUP files from Windows Update servers with ease. This project is not affiliated with Microsoft Corporation.';
-$s['notAffiliated'] = 'This project is not affiliated with Microsoft Corporation. Windows is a registered trademark of Microsoft Corporation.';
-$s['copyright'] = '© %d %s and contributors.'; //© 2019 whatever127 and contributors.
-$s['selectLanguage'] = 'Please select your language';
+$s['home'] = '首頁';
+$s['downloads'] = '下載';
+$s['lightMode'] = '亮色模式';
+$s['darkMode'] = '暗色模式';
+$s['sourceCode'] = '原始程式碼';
+$s['menu'] = '選單';
+$s['websiteDesc'] = '輕鬆地從 Windows Update 伺服器下載 UUP 檔案。這個專案與 Microsoft Corporation 無關。';
+$s['notAffiliated'] = '這個專案與 Microsoft Corporation 無關。Windows 是 Microsoft Corporation 的註冊商標。';
+$s['copyright'] = '© %d %s 和貢獻者們。'; //© 2019 whatever127 and contributors.
+$s['selectLanguage'] = '請選擇語言';
 
 //index.php
-$s['slogan'] = 'Download UUP files from Windows Update servers with ease.';
-$s['advOptions'] = 'Advanced options';
-$s['browseBuilds'] = 'Browse the list of known builds';
-$s['browseBuildsSub'] = 'Choose a build that is already known in the local database and download it.';
-$s['fetchLatest'] = 'Fetch the latest build';
-$s['fetchLatestSub'] = 'Retrieve the latest build information from Windows Update servers.';
-$s['newlyAdded'] = 'Newly added builds';
-$s['dateAdded'] = 'Date added';
+$s['slogan'] = '輕鬆地從 Windows Update 伺服器下載 UUP 檔案。';
+$s['advOptions'] = '進階選項';
+$s['browseBuilds'] = '瀏覽已知組建列表';
+$s['browseBuildsSub'] = '選擇已存在於本機資料庫的組建並下載。';
+$s['fetchLatest'] = '取得最新組建';
+$s['fetchLatestSub'] = '從 Windows Update 伺服器取得最新的組建資訊。';
+$s['newlyAdded'] = '新增加的組建';
+$s['dateAdded'] = '加入日期';
 
 //known.php
-$s['browseKnown'] = 'Browse known builds';
-$s['chooseBuild'] = 'Choose build';
-$s['weFoundBuilds'] = 'We have found <b>%d</b> builds for your query.'; //We have found <b>692</b> builds for your query.
+$s['browseKnown'] = '瀏覽已知組建';
+$s['chooseBuild'] = '選擇組建';
+$s['weFoundBuilds'] = '我們已依您的搜尋內容找到 <b>%d</b> 個組建。'; //We have found <b>692</b> builds for your query.
 
 //latest.php
-$s['latestFetchLatest'] = 'Fetch the latest build';
-$s['latestTestingOnly'] = 'Testing purposes only';
-$s['latestTestingOnlyWarn'] = '<b>This page is provided for testing purposes only.</b> Builds retrieved by this page that were not processed by the backend server will be provided using fallback packs, which may provide incomplete results. If you want to download an already known build, please use the known builds page instead.';
-$s['latestDoYouWantKnown'] = 'Would you like to continue by browsing the list of known builds?';
-$s['chooseOptions'] = 'Choose options';
-$s['buildOfPretendedClient'] = 'Build number of mock Windows Update client';
-$s['editionOfPretendedClient'] = 'Edition of mock system';
+$s['latestFetchLatest'] = '取得最新組建';
+$s['latestTestingOnly'] = '僅供測試用途';
+$s['latestTestingOnlyWarn'] = '<b>這個頁面僅供測試用途。</b> 從這個頁面取得之後端伺服器未處理的組建版本 server will be provided using fallback packs, which may provide incomplete results. If you want to download an already known build, please use the known builds page instead.';
+$s['latestDoYouWantKnown'] = '請問你是否要瀏覽已知組件列表？';
+$s['chooseOptions'] = '選擇選項';
+$s['buildOfPretendedClient'] = 'mock Windows Update 客戶端的組建號碼';
+$s['editionOfPretendedClient'] = 'mock 系統版本';
 $s['skipAheadLabel'] = 'Skip ahead flight';
-$s['skipAheadOption'] = 'Use skip ahead flighting (Insider Fast only)';
-$s['fetchUpdates'] = 'Fetch updates';
-$s['fetchUpdatesInfo'] = 'Click the <i>Fetch updates</i> button to send your request to the Windows Update servers.';
+$s['skipAheadOption'] = '使用 Skip ahead flight (僅 Insider Fast 可用)';
+$s['fetchUpdates'] = '取得更新';
+$s['fetchUpdatesInfo'] = '按下 [取得更新] 按鈕以傳送請求至 Windows Update 伺服器。';
 
 //fetchupd.php
-$s['responseFromServer'] = 'Response from the server';
-$s['foundUpdates'] = 'Found %d update(s)'; //Found 1 update(s)
-$s['foundTheseUpdates'] = 'The following updates were found. Click the name of your desired update to continue.';
-$s['buildNumber'] = 'Build number: %s'; //Build number: 18890.1000
+$s['responseFromServer'] = '伺服器回應';
+$s['foundUpdates'] = '找到 %d 個更新'; //Found 1 update(s)
+$s['foundTheseUpdates'] = '找到以下更新。請按下你想要的更新後繼續。';
+$s['buildNumber'] = '組建版本: %s'; //Build number: 18890.1000
 
 //selectlang.php
-$s['selectLangFor'] = 'Select language for %s'; //Select language for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
-$s['chooseLang'] = 'Choose language';
-$s['chooseLangDesc'] = 'Choose your desired language';
-$s['allLangs'] = 'All languages';
-$s['selLangFiles'] = 'Files';
-$s['allFiles'] = 'All files';
-$s['wubOnly'] = 'WindowsUpdateBox only';
-$s['updateOnly'] = 'Update only';
-$s['selectLangInfoText1'] = 'Click the <i>Next</i> button to select the edition you want to download.';
-$s['selectLangInfoText2'] = 'WindowsUpdateBox.exe and Cumulative updates can be found in the <i>All languages</i> language.';
-$s['allLangsWarn'] = 'The <i>All languages</i> option does not support edition selection.';
-$s['clickNextToOpenFindFiles'] = 'Click the <i>Next</i> button to open the page, which allows finding files.';
-$s['noLangsAvailable'] = 'There are no languages available for this build.';
-$s['browseFiles'] = 'Browse files';
-$s['browseFilesDesc'] = 'Quickly browse files in selected build';
-$s['searchFiles'] = 'Search files';
-$s['toSearchForCUUseQuery'] = 'To search for Cumulative Updates use the <i>%s</i> search query.'; //To search for Cumulative Updates use the <i>Windows10 KB</i> search query.
+$s['selectLangFor'] = '選擇 %s 的語言'; //Select language for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
+$s['chooseLang'] = '選擇語言';
+$s['chooseLangDesc'] = '請選擇你想要的語言';
+$s['allLangs'] = '所有語言';
+$s['selLangFiles'] = '檔案';
+$s['allFiles'] = '所有檔案';
+$s['wubOnly'] = '僅 WindowsUpdateBox';
+$s['updateOnly'] = '僅更新';
+$s['selectLangInfoText1'] = '按一下 [下一步] 按鈕以選擇您想下載的版本。';
+$s['selectLangInfoText2'] = 'WindowsUpdateBox.exe 及累積更新可在 [所有語言] 語言找到。';
+$s['allLangsWarn'] = '[所有語言] 選項不支援版本選取。';
+$s['clickNextToOpenFindFiles'] = '按下 [下一步] 按鈕開啟用來搜尋檔案的頁面。';
+$s['noLangsAvailable'] = '這個組建沒有可供使用的語言。';
+$s['browseFiles'] = '瀏覽檔案';
+$s['browseFilesDesc'] = '快速瀏覽選取組建版本的檔案';
+$s['searchFiles'] = '搜尋檔案';
+$s['toSearchForCUUseQuery'] = '請搜尋 [%s] 以尋找累積更新。'; //To search for Cumulative Updates use the <i>Windows10 KB</i> search query.
 
 //selectedition.php
-$s['selectEditionFor'] = 'Select edition for %s'; //Select edition for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64, English (United States)
-$s['chooseEdition'] = 'Choose edition';
-$s['chooseEditionDesc'] = 'Choose your desired edition';
-$s['allEditions'] = 'All editions';
-$s['selectEditionInfoText'] = 'Click the <i>Next</i> button to open the summary page of your selection.';
-$s['additionalEditionsInfo'] = 'If you need an additional edition which can be found in the table on the right, please select the indicated required edition and click <i>Next</i>. On the summary page you will be able to select your desired additional editions under the appropriate download option.';
+$s['selectEditionFor'] = '選取 %s 的版本'; //Select edition for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64, English (United States)
+$s['chooseEdition'] = '選取版本';
+$s['chooseEditionDesc'] = '選取您想要的版本';
+$s['allEditions'] = '所有版本';
+$s['selectEditionInfoText'] = '按下 [下一步] 按鈕開啟選擇項目的摘要頁面。';
+// TRANTAG: [Fuzzy]
+$s['additionalEditionsInfo'] = '如果你需要可以在右方表格找到的額外版本，請先選擇指示的所需版本後按下 [下一步]。你將可以在摘要頁面的合適下載選項下方選擇所想要的額外版本。';
 
 //download.php
-$s['summary'] = 'Summary';
-$s['summaryDesc'] = 'Review your selection and choose the download method';
-$s['summaryFor'] = 'Summary for %s'; //Summary for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64, English (United States), Windows 10 Pro
-$s['summaryOfSelection'] = 'Summary of your selection';
-$s['browseList'] = 'Browse a list of files';
-$s['browseListDesc'] = 'Opens the page with a list of files in the UUP set for manual download.';
-$s['aria2Opt1'] = 'Download using aria2';
-$s['aria2Opt1Desc'] = 'Easily download the selected UUP set using aria2.';
-$s['aria2Opt2'] = 'Download using aria2 and convert';
-$s['aria2Opt2Desc'] = 'Easily download the selected UUP set using aria2 and convert it to ISO.';
-$s['aria2Opt3'] = 'Download using aria2, convert and create additional editions';
-$s['aria2Opt3Desc'] = 'Easily download the selected UUP set using aria2, convert, create additional editions and finally create an ISO image.';
-$s['jsRequiredToConf'] = 'JavaScript is required to configure and use this option.';
-$s['selAdditionalEditions'] = 'Select additional editions';
-$s['noAdditionalEditions'] = 'No additional editions are available for this selection.';
-$s['learnMore'] = 'Learn more';
-$s['learnMoreAdditionalEditions1'] = 'This option enables automatic creation of selected additional editions.';
-$s['learnMoreAdditionalEditions2'] = 'The list of additional editions is determined by the selected base editions. Below you can check the list of base editions which are needed to create the desired additional editions:';
-$s['learnMoreUpdates1'] = 'Updates will be integrated to the converted image only when the conversion script is run on the following systems:';
-$s['learnMoreUpdates2'] = 'If you run the conversion script on any other system, the updates will not be integrated to the resulting image.';
-$s['systemWithAdk'] = '%s with Windows 10 ADK installed'; //Windows 7 with Windows 10 ADK installed
-$s['additionalUpdates'] = 'Additional updates';
-$s['additionalUpdatesDesc'] = 'This UUP set contains additional updates which will be integrated during the conversion process, significantly increasing the creation time.';
-$s['browseUpdatesList'] = 'Browse the list of updates';
+$s['summary'] = '摘要';
+$s['summaryDesc'] = '再次確認您的選擇，並選擇下載方式';
+$s['summaryFor'] = '%s 的摘要'; //Summary for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64, English (United States), Windows 10 Pro
+$s['summaryOfSelection'] = '選擇項目的摘要';
+$s['browseList'] = '瀏覽檔案列表';
+// TRANTAG: [Fuzzy]
+$s['browseListDesc'] = '開啟要手動下載之包含在 UUP 集合中檔案的列表。';
+$s['aria2Opt1'] = '使用 aria2 下載';
+$s['aria2Opt1Desc'] = '使用 aria2 簡單地下載選取的 UUP 集合。';
+$s['aria2Opt2'] = '使用 aria2 下載並轉換';
+$s['aria2Opt2Desc'] = '使用 aria2 簡單地下載選取的 UUP 集合並轉換成 ISO。';
+$s['aria2Opt3'] = '使用 aria2 下載、轉換並建立額外版本';
+$s['aria2Opt3Desc'] = '使用 aria2 簡單地下載選取的 UUP 集合，再轉換及建立額外版本，最後建立 ISO 映像。';
+$s['jsRequiredToConf'] = '設定及使用此選項需要 JavaScript。';
+$s['selAdditionalEditions'] = '選擇額外版本';
+$s['noAdditionalEditions'] = '此選取項目沒有可供使用的額外版本。';
+$s['learnMore'] = '了解更多';
+$s['learnMoreAdditionalEditions1'] = '這個選項會啟用自動建立選擇的額外版本。';
+$s['learnMoreAdditionalEditions2'] = '這個額外版本列表基於選取的基礎版本。你能在底下查看要建立所需額外版本所需要的版本：';
+$s['learnMoreUpdates1'] = '當轉換指令碼在以下系統執行時，更新將只會整合至轉換映像：';
+$s['learnMoreUpdates2'] = '如果在其他系統執行轉換指令碼，更新將不會整合至結果映像。';
+$s['systemWithAdk'] = '%s，且安裝了 Windows 10 ADK'; //Windows 7 with Windows 10 ADK installed
+$s['additionalUpdates'] = '額外更新';
+$s['additionalUpdatesDesc'] = '這個 UUP 集合包含了會在轉換程序整合進去的額外更新，將會顯著增加建立時間。';
+$s['browseUpdatesList'] = '瀏覽更新列表';
 
 //get.php
-$s['listOfFilesFor'] = 'List of files for %s'; //List of files for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
-$s['totalSizeOfFiles'] = 'Total size of files: %s'; //Total size of files: 2.86 GiB
-$s['fileRenamingScript'] = 'File renaming script';
-$s['fileRenamingScriptDesc1'] = 'The script found below can be used to quickly rename downloaded files.';
-$s['fileRenamingScriptDesc2'] = 'Simply copy the contents of the form below to a new file with <code>cmd</code> extension, put it in folder with downloaded files and run.';
-$s['sha1File'] = 'SHA-1 checksums file';
-$s['sha1FileDesc'] = 'You can use this file to quickly verify that files were downloaded correctly.';
+$s['listOfFilesFor'] = '列出 %s 的檔案'; //List of files for Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
+$s['totalSizeOfFiles'] = '檔案總計大小: %s'; //Total size of files: 2.86 GiB
+$s['fileRenamingScript'] = '檔案重新命名指令碼';
+$s['fileRenamingScriptDesc1'] = '下方所找到的指令碼可用來快速地重新命名下載檔案。';
+$s['fileRenamingScriptDesc2'] = '簡單地將下方表單的內容複製成一個新檔案，副檔名為 <code>cmd</code>，接著把檔案放在下載檔案的資料夾內，之後執行。';
+$s['sha1File'] = 'SHA-1 檔案總和檢查';
+$s['sha1FileDesc'] = '你可以使用這個檔案簡單地驗證下載的檔案是否正確。';
 $s['aria2NoticeTitle'] = 'Download using aria2 options notice';
 $s['aria2NoticeText1'] = 'Download using aria2 options create an archive which needs to be downloaded. The downloaded archive contains all needed files to achieve the selected task.';
-$s['aria2NoticeText2'] = 'To start the download process use a script for your platform:';
-$s['aria2NoticeText3'] = 'Aria2 is an open source project. You can find it here: %s.'; //Aria2 is an open source project. You can find it here: https://aria2.github.io/.
-$s['aria2NoticeText4'] = 'The UUP Conversion script (Windows version) has been created by %s.'; //UUP Conversion script (Windows version) has been created by abbodi1406.
-$s['aria2NoticeText5'] = 'The UUP Conversion script (Linux version) is open source. You can find it here: %s.'; //UUP Conversion script (Linux version) is open source. You can find it here: https://github.com/uup-dump/converter.
+$s['aria2NoticeText2'] = '請使用適用於您的平台的指令碼來開始下載程序:';
+$s['aria2NoticeText3'] = 'Aria2 是個開放原始碼的專案。你可以在這裡找到: %s.'; //Aria2 is an open source project. You can find it here: https://aria2.github.io/.
+$s['aria2NoticeText4'] = 'UUP 轉換指令碼 (Windows 版本) 是由 %s 製作的。'; //UUP Conversion script (Windows version) has been created by abbodi1406.
+$s['aria2NoticeText5'] = 'UUP 轉換指令碼 (Linux 版本) 是開放原始碼的。你可以在這裡找到: %s。'; //UUP Conversion script (Linux version) is open source. You can find it here: https://github.com/uup-dump/converter.
 
 //findfiles.php
 $s['findFilesIn'] = 'Find files in %s'; //Find files in Windows 10 Insider Preview 18890.1000 (rs_prerelease) amd64
