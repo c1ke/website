ca122c2f1dd268d994bc43c3607edff6cbd5818b
whatever127 <mkuba50@outlook.com>
2019-11-09 22:17:43 +0100

    Bump version

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 // Website information
-$websiteVersion = '3.33.4';
+$websiteVersion = '3.33.5';
 $requiredApi = '1.25.0';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
