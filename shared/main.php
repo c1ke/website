f93fabc94601f12ca9de79c87c659ca8d3a2f0a9
whatever127 <46111995+whatever127@users.noreply.github.com>
2020-02-02 19:33:35 +0100

    Bump version

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 // Website information
-$websiteVersion = '3.36.3';
+$websiteVersion = '3.36.4';
 $requiredApi = '1.28.0';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
