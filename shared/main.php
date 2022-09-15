60a3a44b6ef17077fc3af83be14a82bd719a5f6d
eraseyourknees <112027564+eraseyourknees@users.noreply.github.com>
2022-09-16 00:48:51 +0200

    3.53.0

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 // Website information
-$websiteVersion = '3.53.0-dev';
+$websiteVersion = '3.53.0';
 $requiredApi = '1.39.0';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
